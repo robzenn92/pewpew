1ecedf29dcbfa791ecbaf4945cbb12dbf220dd53
Stefano Contiero <stefanocontiero@me.com>
2013-07-20 16:52:43 +0200

    Frontend v4

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,3 +1,12 @@
+function mainCtrl($scope) {
+	$scope.artist = "Artist";
+	$scope.title = "Title";
+	$scope.mood = "Relaxed";
+	$scope.color = "#9eb7ee";
+	$scope.image = "http://farm8.staticflickr.com/7106/7557578150_8249cf6b06_b.jpg";
+	$scope.audio = "http://tympanus.net/Development/AudioPlayer/audio/BlueDucks_FourFlossFiveSix.mp3";
+}
+
 $(function() {
     $('audio').audioPlayer();
 });
