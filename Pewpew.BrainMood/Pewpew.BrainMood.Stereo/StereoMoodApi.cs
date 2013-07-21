12e0812ee83b0fb7cc3ee78ed098e927a65a10cd
Alberto Codutti <Coduz.spam@gmail.com>
2013-07-21 12:56:16 +0200

    Fixed track selection in StereoMoodApi

--- Pewpew.BrainMood/Pewpew.BrainMood.Stereo/StereoMoodApi.cs
+++ Pewpew.BrainMood/Pewpew.BrainMood.Stereo/StereoMoodApi.cs
@@ -9,79 +9,85 @@ using System.Threading.Tasks;
 
 namespace Pewpew.BrainMood.Stereo
 {
-	public class StereoMoodApi
-	{
-		private static Dictionary<string, int> SongIndexes { get; set; }
-		private static Dictionary<string, int> TotalPages { get; set; }
-		private const string ApiKey = "201106acca1d39ef81f8ab793c51699a051e5080e";
+    public class StereoMoodApi
+    {
+        private static Dictionary<string, int> SongIndexes { get; set; }
+        private static Dictionary<string, int> TotalPages { get; set; }
+        private const string ApiKey = "201106acca1d39ef81f8ab793c51699a051e5080e";
 
-		public StereoMoodApi()
-		{
-			SongIndexes = new Dictionary<string, int>();
-			SongIndexes["CONFUSED"] = 0;
-			SongIndexes["CONTEMPLATIVE"] = 0;
-			SongIndexes["FOCUSED"] = 0;
-			SongIndexes["THOUGHTFUL"] = 0;
+        public StereoMoodApi()
+        {
+            SongIndexes = new Dictionary<string, int>();
+            SongIndexes["CONFUSED"] = 0;
+            SongIndexes["CONTEMPLATIVE"] = 0;
+            SongIndexes["FOCUSED"] = 0;
+            SongIndexes["THOUGHTFUL"] = 0;
 
-			TotalPages = new Dictionary<string, int>();
-			TotalPages["CONFUSED"] = 1;
-			TotalPages["CONTEMPLATIVE"] = 4;
-			TotalPages["FOCUSED"] = 1;
-			TotalPages["THOUGHTFUL"] = 3;
-		}
+            TotalPages = new Dictionary<string, int>();
+            TotalPages["CONFUSED"] = 1;
+            TotalPages["CONTEMPLATIVE"] = 4;
+            TotalPages["FOCUSED"] = 1;
+            TotalPages["THOUGHTFUL"] = 3;
+        }
 
-		public string GetSong(String mood) {
-			switch (mood)
-			{
-				case "CONFUSED":
-					return this.GetSong(Moods.type.CONFUSED);
-				case "CONTEMPLATIVE":
-					return this.GetSong(Moods.type.CONTEMPLATIVE);
-				case "FOCUSED":
-					return this.GetSong(Moods.type.FOCUSED);
-				case "THOUGHTFUL":
-					return this.GetSong(Moods.type.THOUGHTFUL);
-				default:
-					return null;
-			}
-		}
+        public string GetSong(String mood)
+        {
+            switch (mood)
+            {
+                case "CONFUSED":
+                    return this.GetSong(Moods.type.CONFUSED);
+                case "CONTEMPLATIVE":
+                    return this.GetSong(Moods.type.CONTEMPLATIVE);
+                case "FOCUSED":
+                    return this.GetSong(Moods.type.FOCUSED);
+                case "THOUGHTFUL":
+                    return this.GetSong(Moods.type.THOUGHTFUL);
+                default:
+                    return null;
+            }
+        }
 
-		public string GetSong(Enum mood)
-		{
-			Random randomizer = new Random();
+        public string GetSong(Enum mood)
+        {
+            Random randomizer = new Random();
 
-			int page = randomizer.Next(0, TotalPages[mood.ToString()]);
+            int page = randomizer.Next(0, TotalPages[mood.ToString()]);
 
-			var url = string.Format(
-				"http://www.stereomood.com/api/search.json?api_key={0}&type=mood&q={1}&page={2}",
-				ApiKey,
-				mood.ToString(),
-				page
-			);
+            var url = string.Format(
+                "http://www.stereomood.com/api/search.json?api_key={0}&type=mood&q={1}&page={2}",
+                ApiKey,
+                mood.ToString(),
+                page
+            );
 
-			WebClient wc = new WebClient();
+            WebClient wc = new WebClient();
 
-			var json_result = wc.DownloadString(url);
+            var json_result = wc.DownloadString(url);
 
-			var reader = new JsonTextReader(new StringReader(json_result));
+            var reader = new JsonTextReader(new StringReader(json_result));
 
-			dynamic target = new JsonSerializer().Deserialize(reader);
+            dynamic target = new JsonSerializer().Deserialize(reader);
 
-			object jsonObject = new
-			{
-				Mood = mood.ToString(),
-				Title = target.songs[SongIndexes[mood.ToString()]].title,
-				Url = target.songs[SongIndexes[mood.ToString()]].audio_url,
-				Artist = target.songs[SongIndexes[mood.ToString()]].artist
-			};
+            if (SongIndexes[mood.ToString()] > target.total)
+            {
+                SongIndexes[mood.ToString()] = 0;
+            }
 
-			SongIndexes[mood.ToString()] += 1;
+            object jsonObject = new
+            {
+                Mood = mood.ToString(),
+                Title = target.songs[SongIndexes[mood.ToString()]].title,
+                Url = target.songs[SongIndexes[mood.ToString()]].audio_url,
+                Artist = target.songs[SongIndexes[mood.ToString()]].artist
+            };
 
-			SongIndexes[mood.ToString()] = SongIndexes[mood.ToString()] % 20;
+            SongIndexes[mood.ToString()] += 1;
 
-			return JsonConvert.SerializeObject(jsonObject);
-		}
-	}
+            SongIndexes[mood.ToString()] = SongIndexes[mood.ToString()] % 20;
+
+            return JsonConvert.SerializeObject(jsonObject);
+        }
+    }
 
 
 }
