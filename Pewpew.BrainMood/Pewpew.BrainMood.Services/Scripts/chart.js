a787b08935bee3df20b745e19277fcbdad5d3c26
Puppo <dp_luca@hotmail.it>
2013-07-21 13:55:25 +0200

    web app

--- Pewpew.BrainMood/Pewpew.BrainMood.Services/Scripts/chart.js
+++ Pewpew.BrainMood/Pewpew.BrainMood.Services/Scripts/chart.js
@@ -4,7 +4,8 @@
 		// be fetched from a server
 
 		var data = [],
-			maxValori = 20;
+			maxValori = 5,
+			res = [];
 
 		function readNewValue() {
 
@@ -13,32 +14,30 @@
 			}
 
 			// Leggo il dato
-			var values = { 
+			var values = {
 				meditazione: 0,
 				attenzione: 0
 			};
-
-			var res = [];
-			$.getJSON("https://github.com/robzenn92/pewpew/blob/master/frontend/file.json", function(asd) {
-				data.push(asd.meditazione);
+			res = [];
+			$.getJSON("/api/graph", function(asd) {
+				data.push(asd.meditation);
 				j = 0;
 				for (var i = data.length - 1; i >= 0 ; i--) {
 					res.push([maxValori - j++, data[i]]);
 				}
+				update();
 			});
-
-			return res;
 		}
 
 		var updateInterval = 5000;
-
-		var plot = $.plot("#chart", [ readNewValue() ], {
+		readNewValue();
+		var plot = $.plot("#chart", [ res ], {
 			series: {
 				shadowSize: 0	// Drawing is faster without shadows
 			},
 			yaxis: {
 				min: 0,
-				max: 100
+				max: 50000
 			},
 			xaxis: {
 				min: 0,
@@ -47,10 +46,8 @@
 		});
 
 		function update() {
-			plot.setData([readNewValue()]);
+			plot.setData([res]);
 			plot.draw();
-			setTimeout(update, updateInterval);
+			setTimeout(readNewValue, updateInterval);
 		}
-
-		update();
 	});
